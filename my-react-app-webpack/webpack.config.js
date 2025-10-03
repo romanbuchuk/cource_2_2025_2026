b6d5ef3c81267dcbd4f811400cfb99ee859f346d
Roman Buchuk <roman.buchuk@sigmatechnology.com>
2025-10-03 12:34:44 +0300

    Added html-webpack-plugin

--- my-react-app-webpack/webpack.config.js
+++ my-react-app-webpack/webpack.config.js
@@ -1,6 +1,20 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
 module.exports = {
     "entry": "./src/index.js",
     "output": {
+        "path": __dirname + "/dist",
         "clean": true,
-    }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.html$/,
+                loader: 'html-loader'
+            }],
+    },
+    plugins: [new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, './src/template/index.html'),
+    })],
 }
