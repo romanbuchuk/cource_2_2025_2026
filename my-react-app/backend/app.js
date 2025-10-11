1bef85a4358388f06b0f5d9aa25769acf9f315b6
Roman Buchuk <roman.buchuk@sigmatechnology.com>
2025-10-11 13:19:51 +0300

    Added simple express

---
+++ my-react-app/backend/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('node:path');
+
+const app = express();
+
+const staticPath = path.join(__dirname, '../frontend/dist');
+
+app.use(express.static(staticPath));
+
+app.use((req, res, next) => {
+    console.log('------------- Express ---------------');
+
+    next();
+});
+
+// app.get('/', (req, res) => {
+//     res.status(200).send('Hello GET request!');
+// });
+
+app.get('/', (req, res) => {
+    res.status(200).sendFile(staticPath);
+});
+
+module.exports = app;
---
+++ my-react-app/backend/index.js
@@ -0,0 +1,6 @@
+const app = require('./app');
+
+// app.listen(8085, function() {});
+app.listen(8085, () => {
+    console.log('------> Listening on 8085 <---------');
+});
